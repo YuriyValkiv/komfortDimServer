ea75599905d9298162f1353dce5beba57ea832db
YuriyValkiv <yurivalkiv@gmail.com>
2019-12-23 23:34:45 +0100

    Add files via upload

---
+++ src/main/java/com/dim/komfort/KomfortApplication.java
@@ -0,0 +1,13 @@
+package com.dim.komfort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KomfortApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KomfortApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/dim/komfort/KomfortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dim.komfort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KomfortApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
