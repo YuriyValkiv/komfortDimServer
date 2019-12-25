6ec373dacb32097daf1a47931cc32ba4c93c49d7
YuriyValkiv <yurivalkiv@gmail.com>
2019-12-25 21:44:14 +0100

    request mapping missing added

--- src/main/java/com/dim/komfort/controller/UserController.java
+++ src/main/java/com/dim/komfort/controller/UserController.java
@@ -5,11 +5,13 @@ import com.dim.komfort.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-@RestController("/")
+@RestController
+@RequestMapping("/")
 public class UserController {
 
   private final UserService userService;
