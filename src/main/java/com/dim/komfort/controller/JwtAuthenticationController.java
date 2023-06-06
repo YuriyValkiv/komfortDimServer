764a0d651c50674edd93cc4af9925b4a27baa348
YuriyValkiv <yurivalkiv@gmail.com>
2023-06-06 19:30:14 +0300

    test new cors on method

--- src/main/java/com/dim/komfort/controller/JwtAuthenticationController.java
+++ src/main/java/com/dim/komfort/controller/JwtAuthenticationController.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,7 @@ public class JwtAuthenticationController {
     this.jwtInMemoryUserDetailsService = jwtInMemoryUserDetailsService;
   }
 
+  @CrossOrigin(value = "https://komfortdim.if.ua")
   @PostMapping(value = "/authenticate")
   public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
           throws Exception {
